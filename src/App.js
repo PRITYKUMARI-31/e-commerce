054e1a0884f65172e6b3baf6a73fe20a9bb4d015
PRITYKUMARI-31 <prity072003@gmail.com>
2024-06-23 13:44:26 +0530

    basic structure ready

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import ShopCategory from './Pages/ShopCategory';
 import LoginSignup from './Pages/LoginSignup.JSX';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
        <Route path='/login' element={<LoginSignup />} />
        
       </Routes>
+      <Footer></Footer>
       </BrowserRouter>
     </div>
   );
